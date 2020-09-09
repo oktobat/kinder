6b629b602a41133bed922e09bec1d886eeb5f8e0
oktobat <62067363+oktobat@users.noreply.github.com>
2020-09-10 00:01:16 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,19 @@
+(function ($) {
+  $("#containerBox").load("main.html");
+
+  $(".depth1 > li").hover(
+    function () {
+      $(this).find(".depth2").stop().slideDown(700);
+    },
+    function () {
+      $(this).find(".depth2").stop().slideUp(500);
+    }
+  );
+
+  $(".login a, h1 a").on("click", function (e) {
+    e.preventDefault();
+    var url = $(this).attr("href");
+    $("#container").remove();
+    $("#containerBox").load(url);
+  });
+})(jQuery);
